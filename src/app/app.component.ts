b3349e577651c626b5f885d4c3de0ddeb7fe6b76
ebuenomat <ebuenomat@gmail.com>
2023-05-24 13:39:33 -0300

    primeira calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { count } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'teste-OAS';
+
+  v1: number = 0;
+  v2: number = 0;
+  r = signal<number>(0) ;
+  rDoubled = computed(() => this.r() * 2)
+
+  operacao: any;
+
+  constructor() { }
+
+  sum() {
+    this.r.update(r => Number(this.v1) + Number(this.v2));
+  }
+
+  sub() {
+    this.r.update(r => Number(this.v1) - Number(this.v2));
+    // this.r = Number(this.v1) - Number(this.v2);
+  }
+
+  mult() {
+    this.r.update(r => Number(this.v1) * Number(this.v2));
+    // this.r = Number(this.v1) * Number(this.v2);
+  }
+
+  div() {
+    if(Number(this.v2) != 0) {
+      this.r.update(r => Number(this.v1) / Number(this.v2));
+      // this.r = Number(this.v1) / Number(this.v2);
+    }
+    else {
+      alert('Não é possível dividir por zero!!');
+    }
+   
+  }
+
+  calcular(operacao: string) {
+
+    switch (operacao) {
+      case 'soma':
+        this.sum();
+      break;
+      case 'subtração':
+        this.sub();
+      break;
+      case 'multiplicação':
+        this.mult();
+      break;
+      case 'divisão':
+        this.div();
+      break;
+
+    }
+
+  }
 }
